57f557a3dea1c9d8aaa58d435459091f2461b84f
agna2103 <161094297+agna2103@users.noreply.github.com>
2024-09-23 09:53:08 +0700

    Update hitung.py

--- hitung.py
+++ hitung.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
 x = st.number_input( " Insert a number ", value = None)
-st.write( " the current number is  = " , x)
+sx = st.text_input(" Satuan ", "C")
+st.write( " the current number is  = " , x, " " ,sx)
 
