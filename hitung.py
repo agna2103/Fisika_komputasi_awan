1414d929ea759dee57f5f8a04cb206304f345e87
agna2103 <161094297+agna2103@users.noreply.github.com>
2024-09-23 09:50:29 +0700

    Update hitung.py

--- hitung.py
+++ hitung.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-x = st.number_input( " Insert a number ", Value = None, placeholder="Tuliskan angka...")
+x = st.number_input( " Insert a number ", Value = None)
 st.write( " the current number is  = " , x)
 
